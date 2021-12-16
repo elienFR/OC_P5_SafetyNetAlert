7da0c94630f0e636624a596db905d497a9676175
Elien <elienferre@yahoo.fr>
2021-12-16 14:30:26 +0100

    base addings

---
+++ dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/DbApiApplication.java
@@ -0,0 +1,13 @@
+package org.safetynet.p5safetynetalert.dbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbApiApplication.class, args);
+	}
+
+}
---
+++ dbapi/src/test/java/org/safetynet/p5safetynetalert/dbapi/DbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.safetynet.p5safetynetalert.dbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
