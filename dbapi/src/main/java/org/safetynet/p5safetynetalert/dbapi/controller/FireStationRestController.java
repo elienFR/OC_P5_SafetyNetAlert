b855600076f58f0ee9848188f4e162b59fdb40af
Elien <elienferre@yahoo.fr>
2021-12-29 18:31:41 +0100

    formatted view for persons from fire station

---
+++ dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/controller/FireStationRestController.java
@@ -0,0 +1,14 @@
+package org.safetynet.p5safetynetalert.dbapi.controller;
+
+import org.safetynet.p5safetynetalert.dbapi.dto.PersonFromFirestationDTO;
+import org.safetynet.p5safetynetalert.dbapi.service.FireStationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FireStationRestController {
+
+
+}
