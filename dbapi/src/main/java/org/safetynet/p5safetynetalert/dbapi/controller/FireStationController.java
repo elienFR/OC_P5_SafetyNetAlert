8dc6e7543bd708a9e264dab565c213f7a43834d7
Elien <elienferre@yahoo.fr>
2021-12-30 12:16:45 +0100

    minor adjustments

--- dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/controller/FireStationController.java
+++ dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/controller/FireStationController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("firestation")
 public class FireStationController {
 
   @Autowired
@@ -18,7 +20,7 @@ public class FireStationController {
   @Autowired
   MajorityCalculatorService majorityCalculatorService;
 
-  @GetMapping("/firestation")
+  @GetMapping("")
   public String getPersonFromFireStationNumber(
       @RequestParam("stationNumber") String fireStationNumber, Model model) throws Exception {
 
--- dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/controller/FireStationRestController.java
+++ dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/controller/FireStationRestController.java
@@ -4,17 +4,19 @@ import org.safetynet.p5safetynetalert.dbapi.dto.PersonFromFirestationDTO;
 import org.safetynet.p5safetynetalert.dbapi.service.FireStationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("firestation-rest")
 public class FireStationRestController {
 
   @Autowired
   FireStationService fireStationService;
 
-  @GetMapping("/testPersonFireStation")
-  public Iterable<PersonFromFirestationDTO> get(@RequestParam("number") String id) {
+  @GetMapping("")
+  public Iterable<PersonFromFirestationDTO> get(@RequestParam("stationNumber") String id) {
     return fireStationService.getPersonDTOFromFireStationId(id);
   }
 }
