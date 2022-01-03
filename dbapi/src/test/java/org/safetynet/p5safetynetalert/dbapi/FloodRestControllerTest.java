062d16d60d08137057334de081aabcf4571c79fd
Elien <elienferre@yahoo.fr>
2022-01-03 17:24:13 +0100

    added flood rest api test

---
+++ dbapi/src/test/java/org/safetynet/p5safetynetalert/dbapi/FloodRestControllerTest.java
@@ -0,0 +1,56 @@
+package org.safetynet.p5safetynetalert.dbapi;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.safetynet.p5safetynetalert.dbapi.controller.FloodRestController;
+import org.safetynet.p5safetynetalert.dbapi.model.dto.FloodPersonsListDTO;
+import org.safetynet.p5safetynetalert.dbapi.service.FloodService;
+import org.safetynet.p5safetynetalert.dbapi.service.initPersist.JsonDataInjectorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = FloodRestController.class)
+public class FloodRestControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private JsonDataInjectorService jsonDataInjectorService;
+
+  @MockBean
+  private FloodService floodService;
+
+  @Test
+  public void getPersonsFloodDTOFromFireStationsTest() throws Exception{
+    Collection<String> stations = new ArrayList<>();
+    for (Integer i = 1; i < 4; i++) {
+      stations.add(i.toString());
+    }
+    FloodPersonsListDTO floodPersonsListDTO = new FloodPersonsListDTO();
+
+    when(floodService.getPersonsFloodDTOFromFireStation(stations)).thenReturn(floodPersonsListDTO);
+
+    MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+      .get("/flood/stations")
+      .param("stations","1")
+//      .param("stations","2")
+//      .param("stations","3")
+      .contentType(MediaType.APPLICATION_JSON);
+
+    mockMvc.perform(builder).andExpect(status().isOk());
+
+  }
+
+}
