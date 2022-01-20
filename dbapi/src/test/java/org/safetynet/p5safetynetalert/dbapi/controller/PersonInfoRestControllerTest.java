49a8eb49ae0775d5952537f1bbda9245b22d4df1
Elien <elienferre@yahoo.fr>
2022-01-20 09:34:57 +0100

    PersonInfoController tests

---
+++ dbapi/src/test/java/org/safetynet/p5safetynetalert/dbapi/controller/PersonInfoRestControllerTest.java
@@ -0,0 +1,10 @@
+package org.safetynet.p5safetynetalert.dbapi.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = PersonInfoRestController.class)
+public class PersonInfoRestControllerTest {
+
+
+
+}
