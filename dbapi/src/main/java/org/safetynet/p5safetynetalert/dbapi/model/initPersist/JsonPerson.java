d18fac6b5ad60941dd67cf0041c2b4501aae71df
Elien <elienferre@yahoo.fr>
2022-01-31 18:38:35 +0100

    added test for the create method in PersonServiceTest

--- dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/model/initPersist/JsonPerson.java
+++ dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/model/initPersist/JsonPerson.java
@@ -15,4 +15,15 @@ public class JsonPerson {
   private String zip;
   private String phone;
   private String email;
+
+  public JsonPerson(){}
+  public JsonPerson(String firstName, String lastName, String address, String city, String zip, String phone, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.zip = zip;
+    this.phone = phone;
+    this.email = email;
+  }
 }
--- dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/service/PersonService.java
+++ dbapi/src/main/java/org/safetynet/p5safetynetalert/dbapi/service/PersonService.java
@@ -1,7 +1,6 @@
 package org.safetynet.p5safetynetalert.dbapi.service;
 
 import com.google.common.collect.Iterables;
-import lombok.Data;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.safetynet.p5safetynetalert.dbapi.model.entity.*;
