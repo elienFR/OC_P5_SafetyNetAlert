26ffc12f80b89c49a00711ce5d65504c3898b67c
Elien <elienferre@yahoo.fr>
2022-02-09 11:28:59 +0100

    erased jacoco exclusion from pom and added test for JsonFileExtractorServiceTest

--- src/test/java/org/safetynet/p5safetynetalert/service/initPersist/JsonFileExtractorServiceTest.java
+++ src/test/java/org/safetynet/p5safetynetalert/service/initPersist/JsonFileExtractorServiceTest.java
@@ -1,38 +1,58 @@
 package org.safetynet.p5safetynetalert.service.initPersist;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
+import org.junit.jupiter.api.Test;
 import org.safetynet.p5safetynetalert.CustomProperties;
+import org.safetynet.p5safetynetalert.model.initPersist.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 
+@SpringBootTest
 public class JsonFileExtractorServiceTest {
 
+  @Autowired
   private IJsonFileExtractorService iJsonFileExtractorServiceUnderTest;
-  @Mock
+
+  @MockBean
+  private IJsonDataInjectorService iJsonDataInjectorService;
+
+  @MockBean
   private CustomProperties customPropertiesMocked;
 
-  @BeforeEach
-  public void initTest(){
-    iJsonFileExtractorServiceUnderTest = new JsonFileExtractorServiceImpl();
+  @Test
+  public void fromFileTest() {
+    //given
+    String givenJsonFileName = "dataTest.json";
+    String givenMainResourcePath = "./src/test/resources/";
+
+    when(customPropertiesMocked.getMainResourcesPath()).thenReturn(givenMainResourcePath);
+
+    //when
+    JsonData result = iJsonFileExtractorServiceUnderTest.fromFile(givenJsonFileName);
+
+    //then
+    assertThat(result.getPersons().getPersons().size()).isGreaterThan(0);
+    assertThat(result.getFireStations().getFirestations().size()).isGreaterThan(0);
+    assertThat(result.getMedicalRecords().getMedicalrecords().size()).isGreaterThan(0);
   }
 
+  @Test
+  public void fromFileTestNull() {
+    //given
+    String givenJsonFileName = "";
+    String givenMainResourcePath = "./src/test/resources/";
 
-//  @Test
-//  public void fromFileTest() {
-//    //given
-//    String givenFileName = "dataTest.json";
-//
-//    when(customPropertiesMocked.getMainResourcesPath()).thenReturn("test");
-//
-//    JsonData expected = new JsonData();
-//
-//    //when
-//    JsonData result = iJsonFileExtractorServiceUnderTest.fromFile(givenFileName);
-//
-//    //then
-//    assertThat(result).isEqualTo(expected);
-//  }
+    when(customPropertiesMocked.getMainResourcesPath()).thenReturn(givenMainResourcePath);
+
+    //when
+    JsonData result = iJsonFileExtractorServiceUnderTest.fromFile(givenJsonFileName);
+
+    //then
+    assertThat(result).isNull();
+  }
 
 }
